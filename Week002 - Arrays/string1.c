cf89951fbe386cc216e055d229eba40481e3f990
mgpeters <matthew.george.peters@gmail.com>
2017-03-16 17:19:51 -0400

    Added string1.c

--- Week002 - Arrays/string1.c
+++ Week002 - Arrays/string1.c
@@ -1,4 +1,5 @@
-// This program outlines how C stores strings as an array.
+// This program outlines how C stores strings as an array, but is version 2 as we
+// optimized the for loop from the previous iteration.
 
 #include <cs50.h>
 #include <stdio.h>
@@ -9,7 +10,7 @@ int main (void){
 
     if (s != NULL){ // The lecture suggests GetString can return NULL if the input is too long. This is a safeguard.
         
-        for (int i = 0; i < strlen(s); i++){
+        for (int i = 0; n = strlen(s); i < n; i++){
             printf("%c\n", s[i]); //prints out each letter in the string
         }
     }
